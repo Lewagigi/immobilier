29b759087b55a5f2b920f9ca50009a931c41b30b
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2024-08-27 17:18:18 +0200

    nii

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 
 <!-- Navigation Bar -->
 <div class="w3-bar w3-white w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-blue w3-mobile"></i>GC</a>
+    <a href="#" class="w3-bar-item w3-button w3-blue w3-mobile">GC</a>
     <a href="#rooms" class="w3-bar-item w3-button w3-mobile">Acceuil</a>
     <a href="#about" class="w3-bar-item w3-button w3-mobile">Le Concept</a>
     <a href="#contact" class="w3-bar-item w3-button w3-mobile">Tarif</a>
@@ -24,13 +24,15 @@
     <a href="#contact" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">S'inscrire</a>
     <a href="#contact" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">Connecter</a>
     <a href="#contact" class="w3-bar-item w3-button w3-right w3-light-grey w3-mobile">Deconnecter</a>
+
+    <img class="w3-image" src="/img/Capture d’écran 2024-08-27 à 13.18.26.png" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
 </div>
 <!-- Header -->
 <header class="w3-display-container w3-content" style="max-width:1500px;">
     <img class="w3-image" src="/img/Capture d’écran 2024-08-27 à 13.18.26.png" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
     <div class="w3-display-right w3-padding w3-col l6 m8">
-        <div class="w3-container w3-blue w3-right" >
-            <h2><i class="fa fa-bed w3-margin-right"></i>Immobilier</h2>
+        <div class="w3-container w3-blue w3-left" >
+            <h2>Immobilier</h2>
         </div>
         <div class="w3-container w3-white w3-padding-16">
             <form action="/action_page.php" target="_blank">
