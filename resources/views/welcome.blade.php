8184defb01f73aa4dd27a618231e168f75400377
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2024-08-29 14:37:07 +0200

    tpptt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,32 +75,9 @@
     <img class="w3-image" src="/img/Capture d’écran 2024-08-27 à 13.18.26.png" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
     <div class="w3-display-right w3-padding w3-col l6 m8">
         <div class="w3-container w3-blue w3-left" >
-            <h2>Immobilier</h2>
-        </div>
-        <div class="w3-container w3-white w3-padding-16">
-            <form action="/action_page.php" target="_blank">
-                <div class="w3-row-padding" style="margin:0 -16px;">
-                    <div class="w3-half w3-margin-bottom">
-                        <label><i class="fa fa-calendar-o"></i>S'enregistrer</label>
-                        <input class="w3-input w3-border" type="text" placeholder="DD MM YYYY" name="CheckIn" required>
-                    </div>
-                    <div class="w3-half">
-                        <label><i class="fa fa-calendar-o"></i>Deconnecter</label>
-                        <input class="w3-input w3-border" type="text" placeholder="DD MM YYYY" name="CheckOut" required>
-                    </div>
-                </div>
-                <div class="w3-row-padding" style="margin:8px -16px;">
-                    <div class="w3-half w3-margin-bottom">
-                        <label><i class="fa fa-male"></i> Adults</label>
-                        <input class="w3-input w3-border" type="number" value="1" name="Adults" min="1" max="6">
-                    </div>
-                    <div class="w3-half">
-                        <label><i class="fa fa-child"></i> Kids</label>
-                        <input class="w3-input w3-border" type="number" value="0" name="Kids" min="0" max="6">
-                    </div>
-                </div>
+
                 <button class="w3-button w3-dark-grey " type="submit"><i class="fa fa-search w3-margin-right"></i> Search availability</button>
-            </form>
+
         </div>
     </div>
 </header>
