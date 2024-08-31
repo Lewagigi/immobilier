4bccc338910b46c236a7b90327a55423bf0b36d1
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2024-08-31 19:26:51 +0200

    newformulaire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,11 +82,17 @@
     </div>
 </header>
 
-<form class="example w3-center" action="/action_page.php" style="margin:auto;max-width:300px; right: 111123px">
+<form class="example w3-left" action="/action_page.php" style="margin:auto;max-width:300px; ">
     <input type="text" placeholder="Search.." name="search2">
     <button type="submit"><i class="fa fa-search"></i></button>
 </form>
 
+
+
+<form class="example w3-right" action="/action_page.php" style="margin:auto;max-width:300px; ">
+    <input type="text" placeholder="Search.." name="search2">
+    <button type="submit"><i class="fa fa-search"></i></button>
+</form>
 <!-- Page content -->
 <div class="w3-content" style="max-width:1532px;">
 
