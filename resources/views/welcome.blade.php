bc774c8cab47615b88f12c5faa3789178c2518af
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2024-09-01 19:00:14 +0200

    uiruiru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,12 +82,14 @@
     </div>
 </header>
 
-<h3 class="gigi">Recherche </h3>
+<h3 class="gigi w3-centre">Recherche </h3>
 
 <form class="example w3-left" action="/action_page.php" style="margin:auto;max-width:300px; ">
     <input type="text" placeholder="Search.." name="search2">
     <button type="submit"><i class="fa fa-search"></i></button>
 </form>
+
+
 
 
 
